1f05867ac1bfc64f9ace3da11c46ec5b3bb68ba2
Ajmal S <ajmalwhlsm@gmail.com>
2026-07-20 17:20:00 +0530

    Add variable declarations and type checks

---
+++ variable_datatype.py
@@ -0,0 +1,25 @@
+#2
+first_name ="john"
+last_name="doe"
+full_name=first_name+" "+last_name
+country="oceania"
+city="meji"
+age=209
+year=2026
+is_married=False
+is_true=True
+is_light_on=True
+-----------
+name,city_name,current_age="Aj","TVM",22
+
+-----------------------
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
