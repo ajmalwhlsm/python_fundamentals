fd6e0acec8e1b45d8dd03ddc6f58bed73367f773
Ajmal S <ajmalwhlsm@gmail.com>
2026-07-26 18:47:55 +0530

    Enhance variable_datatype.py with print statements
    Added various print statements to display variable values and types, along with string manipulations and arithmetic operations.

--- variable_datatype.py
+++ variable_datatype.py
@@ -11,7 +11,6 @@ is_true=True
 is_light_on=True
 -----------
 name,city_name,current_age="Aj","TVM",22
-
 -----------------------
 print(type(first_name))
 print(type(last_name))
@@ -23,3 +22,43 @@ print(type(year))
 print(type(is_married))
 print(type(is_true))
 print(type(is_light_on))
+------------------------
+print(first_name)
+print(last_name)
+print(full_name)
+print(country)
+print(city)
+print(age)
+print(year)
+print(len(first_name))
+print(len(full_name))
+
+print(first_name.upper())
+print(last_name.capitalize())
+print(full_name.title())
+print(first_name[0])
+print(last_name[-1])
+print(full_name.split())
+print(full_name.replace("john","Jane"))
+print(age+1)
+print(age-10)
+print(age*2)
+print(age/2)
+print(age>100)
+print(year==2026)
+print(is_married)
+print(not is_married)
+print(is_true and is_light_on)
+print(type(name))
+print(type(city_name))
+print(type(current_age))
+
+print(name)
+print(city_name)
+print(current_age)
+print(name.upper())
+print(city_name.lower())
+
+print(f"My name is {full_name}")
+print(f"I live in {city},{country}")
+print(f"I am {age} years old")
